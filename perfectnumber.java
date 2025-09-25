e14e59e8a29aceecc29e657b337130c549582eeb
sahanaraja56-ui <sahanaraja56@gmail.com>
2025-09-25 16:36:38 +0530

    Add files via upload

---
+++ perfectnumber.java
@@ -0,0 +1,24 @@
+package com.p1;
+
+public class perfectnumber{
+
+	public static void main(String[] args){
+	int n=7;
+	int sum=0;
+	int i=1;
+	while(n<n/2){
+		if(n%i==0){
+			sum+=i;
+		}
+		i++;
+	}
+	if(sum==n){
+		System.out.println("perfect number");
+	}
+	else{
+		System.out.println("not");
+	}
+	// TODO Auto-generated method stub
+	
+	}
+}
